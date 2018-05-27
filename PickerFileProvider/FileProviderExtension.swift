4ecdcfcecb929b457ab1716777ef749e202446f2
Marino Faggiana <m.faggiana@twsweb.it>
2018-05-27 17:41:15 +0200

    dev

--- PickerFileProvider/FileProviderExtension.swift
+++ PickerFileProvider/FileProviderExtension.swift
@@ -51,7 +51,7 @@ var timerUpload: Timer?
  
    -------------------------------------------------------------------------------------------------------------------------------------------- */
 
-class FileProvider: NSFileProviderExtension, CCNetworkingDelegate {
+class FileProviderExtension: NSFileProviderExtension, CCNetworkingDelegate {
     
     var fileManager = FileManager()
     var providerData = FileProviderData()
